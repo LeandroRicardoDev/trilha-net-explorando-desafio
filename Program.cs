61a48cb470239bf7032a7d11aed227cb90e5e6fb
Leandro Ricardo <leandroricardo@live.com>
2022-09-26 14:48:05 -0300

    Adicionado Menu

--- Program.cs
+++ Program.cs
@@ -5,23 +5,56 @@ Console.OutputEncoding = Encoding.UTF8;
 
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
+Pessoa pessoa = new Pessoa();
+Reserva reserva = new Reserva();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+// Menu
+Console.WriteLine("Menu do Projeto de Hospedagem");
+Console.WriteLine("Digite o que deseja fazer:");
+Console.WriteLine("---------------------");
+Console.WriteLine("1 - Adicionar Hóspedes");
 
-hospedes.Add(p1);
-hospedes.Add(p2);
+Console.WriteLine();
+Console.Write("Digite o número correspondente: ");
+int Menu = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+int diasReservados = 0;
+if (Menu == 1)
+{
+    Console.WriteLine("Quantos Hóspedes deseja adicionar?");
+
+    int quantidadeHospedes = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+    if (quantidadeHospedes <= 2)
+    {
+        for (int i = 1; i <= quantidadeHospedes; i++)
+        {
+            Console.WriteLine("Nome do hóspede: " + i);
+            pessoa = new Pessoa(Console.ReadLine());
+            hospedes.Add(pessoa);
+            Console.WriteLine();
+
+        }
+        Console.WriteLine($"Quantos dias de reserva?");
+        diasReservados = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+    }
+    else
+    {
+        throw new Exception("Esta suíte não está disponível devido a quantidade de hóspedes, favor trocar a suíte.");
+    }
+}
 
 // Cria a suíte
 Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 10);
+reserva = new Reserva(diasReservados);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
 // Exibe a quantidade de hóspedes e o valor da diária
 Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
-
-
+Console.WriteLine($"Valor total das diárias: {reserva.CalcularValorDiaria()}");
+Console.WriteLine();
